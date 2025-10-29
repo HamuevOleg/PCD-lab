cb722d44ea2e3f9b2f43ed7b6b612ebf063c9c27
HamuevOleg <oleg.hamuev@iis.utm.md>
2025-10-29 17:31:07 +0200

    initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class Interval implements Runnable{
+    int start;
+    int end;
+    Interval(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+
+    public void run(start, end){
+
+    }
+
+
+}
